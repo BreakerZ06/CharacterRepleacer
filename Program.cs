59aabd40b1572ac2e1f1eadeedeb2e4414a64d07
Safa Gedik <sgc.breaker@gmail.com>
2023-11-03 01:19:30 +0300

    added: string and character input from user main while loop added

--- Program.cs
+++ Program.cs
@@ -4,7 +4,23 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.Title = "Character Replacement";
+            Console.WriteLine("Type string for process repleacement\n" +
+                "type chars to be repleaced\n" +
+                "type new chars\n" +
+                "type exit for close program\n" +
+                "press enter for start");
+            while (Console.ReadLine()?.Trim() != "exit")
+            {
+                Console.WriteLine("Processing");
+                var str = Console.ReadLine();
+                var oldChar = Console.ReadLine();
+                var newChar = Console.ReadLine();
+                if (str == null || oldChar == null || newChar == null)
+                    throw new Exception("invalid data");
+                Console.WriteLine(str.Replace(oldChar, newChar));
+                Console.WriteLine("Type 'exit' for close program press enter for another repleacement");
+            }
         }
     }
 }
